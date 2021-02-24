a78cecd92a552f00da586dc718755115c847e694
SilverHawks <thekeke60@gmail.com>
2021-02-24 02:11:18 +0100

    Update config.lua

--- simple_magic_system/lua/magicsystem/config.lua
+++ simple_magic_system/lua/magicsystem/config.lua
@@ -23,10 +23,12 @@ SMS_Config = {}
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 SMS_Config.MagieTable = {
-    {"Sans Magie", {"manhack_welder", 0}, {"weapon_medkit", 0}, {"weapon_bugbait", 0}, {"weapon_crowbar", 300} }
+    {"Magie1", {"manhack_welder", 0}, {"weapon_medkit", 0}, {"weapon_bugbait", 0}, {"weapon_crowbar", 300} },
+    {"Magie2", {"manhack_welder", 0}, {"weapon_medkit", 0}, {"weapon_bugbait", 0}, {"weapon_crowbar", 300} },
+    {"Magie3", {"manhack_welder", 0}, {"weapon_medkit", 0}, {"weapon_bugbait", 0}, {"weapon_crowbar", 300} }
 }
-SMS_Config.MagieWeapon = {"manhack_welder", "weapon_medkit", "weapon_bugbait", "weapon_crowbar"}
-SMS_Config.MagieNumber = 1
+SMS_Config.MagieWeapon = {"manhack_welder", "weapon_medkit", "weapon_bugbait", "weapon_crowbar"} -- Add all swep for mana use
+SMS_Config.MagieNumber = 3
 
 -- Mana de la personne de base
 SMS_Config.DefaultMana = 700
@@ -35,6 +37,6 @@ SMS_Config.ManaToSWep2 = 3000
 SMS_Config.ManaToSWep3 = 6000
 SMS_Config.ManaToSWep4 = 9000
 
-SMS_Config.ManaPerMinute = 5
+SMS_Config.ManaPerMinute = 1
 
 SMS_Config.UseULX = false
